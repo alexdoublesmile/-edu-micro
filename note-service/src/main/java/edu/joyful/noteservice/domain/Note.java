c5c5e074fe7fc19e92558cec9dc7349a2f77ade8
Александр Плохой <alexplohoy@mail.ru>
2023-02-11 20:00:30 +0300

    add notes domain

---
+++ note-service/src/main/java/edu/joyful/noteservice/domain/Note.java
@@ -0,0 +1,25 @@
+package edu.joyful.noteservice.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "notes")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    private Long personId;
+}
