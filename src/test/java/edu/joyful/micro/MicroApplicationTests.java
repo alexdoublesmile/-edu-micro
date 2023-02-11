b471a24a4a59a7c161ad54caf23045e78b76c285
Александр Плохой <alexplohoy@mail.ru>
2023-02-11 11:19:10 +0300

    Initial commit

---
+++ src/test/java/edu/joyful/micro/MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.joyful.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
