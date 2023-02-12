a8b965c129fe7f329db144fc89e3c5619d990caf
Александр Плохой <alexplohoy@mail.ru>
2023-02-12 16:40:44 +0300

    add delay for request

--- person-service/src/main/java/edu/joyful/micro/controller/PersonController.java
+++ person-service/src/main/java/edu/joyful/micro/controller/PersonController.java
@@ -3,14 +3,19 @@ package edu.joyful.micro.controller;
 import edu.joyful.micro.domain.Person;
 import edu.joyful.micro.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping("/persons")
 @RequiredArgsConstructor
 public class PersonController {
+    private int DELAY = 500;
+
     private final PersonRepository personRepository;
 
     @GetMapping
@@ -23,8 +28,15 @@ public class PersonController {
         return personRepository.save(person);
     }
 
+    @SneakyThrows
     @GetMapping("/{id}")
     public Person findPersonById(@PathVariable Long id) {
-        return personRepository.findById(id).orElseThrow();
+        log.info("Delaying for {}ms", DELAY += 50);
+        Thread.sleep(DELAY);
+
+        log.info("Fall down :(");
+        throw new RuntimeException();
+
+//        return personRepository.findById(id).orElseThrow();
     }
 }
