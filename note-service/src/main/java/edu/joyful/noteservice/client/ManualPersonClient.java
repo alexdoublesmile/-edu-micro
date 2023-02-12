b862dbfca57dbf8eeb875d510a08d4543d16d0ab
Александр Плохой <alexplohoy@mail.ru>
2023-02-12 16:36:47 +0300

    add circuitbreaker

--- note-service/src/main/java/edu/joyful/noteservice/client/ManualPersonClient.java
+++ note-service/src/main/java/edu/joyful/noteservice/client/ManualPersonClient.java
@@ -1,9 +1,15 @@
 package edu.joyful.noteservice.client;
 
 import edu.joyful.noteservice.domain.dto.PersonDto;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -14,10 +20,19 @@ import org.springframework.web.client.RestTemplate;
 public class ManualPersonClient {
     private final DiscoveryClient discoveryClient;
     private final RestTemplate restTemplate;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
+//    @Retry(name = "person-service")
+//    @TimeLimiter(name = "person-service")
+//    @RateLimiter(name = "person-service")
+//    @Bulkhead(name = "person-service")
+//    @CircuitBreaker(name = "person-service")
     public PersonDto getPersonById(Long personId) {
         final ServiceInstance personInstance = discoveryClient.getInstances("person-service").get(0);
         final String serviceUrl = personInstance.getUri() + "/persons/{personId}";
-        return restTemplate.getForObject(serviceUrl, PersonDto.class, personId);
+
+        return circuitBreakerFactory.create("person-service")
+                .run(() -> restTemplate.getForObject(serviceUrl, PersonDto.class, personId));
+//        return restTemplate.getForObject(serviceUrl, PersonDto.class, personId);
     }
 }
