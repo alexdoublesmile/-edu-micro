895589976567da67ca670a97d377c8e0657c9189
Александр Плохой <alexplohoy@mail.ru>
2023-02-12 10:23:16 +0300

    add person service impl

--- person-service/src/main/java/edu/joyful/micro/service/PersonService.java
+++ person-service/src/main/java/edu/joyful/micro/service/PersonService.java
@@ -2,11 +2,14 @@ package edu.joyful.micro.service;
 
 import edu.joyful.micro.client.NotesClient;
 import edu.joyful.micro.domain.Person;
+import edu.joyful.micro.domain.dto.NoteDto;
 import edu.joyful.micro.domain.dto.PersonDto;
 import edu.joyful.micro.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Log4j2
 @Service
@@ -18,12 +21,12 @@ public class PersonService {
     public PersonDto findPersonWithNotes(Long personId) {
         final Person personFromDB = personRepository.findById(personId).orElseThrow();
 
-
+        final List<NoteDto> notesFromNotesService = notesClient.getNotesByPersonId(personId);
 
         return PersonDto.builder()
                 .firstName(personFromDB.getFirstName())
                 .lastName(personFromDB.getLastName())
-//                .notes(notesFromNotesService)
+                .notes(notesFromNotesService)
                 .build();
     }
 }
