98c33ef61256e99f6875cf5bcff5f6620d190c8e
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 13:57:39 +0300

    fix feign config for auth

--- note-service/src/main/java/edu/joyful/noteservice/config/PersonFeignConfig.java
+++ note-service/src/main/java/edu/joyful/noteservice/config/PersonFeignConfig.java
@@ -2,8 +2,13 @@ package edu.joyful.noteservice.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
 public class PersonFeignConfig {
+
+    
+
+    @Bean
     public BasicAuthRequestInterceptor feignAuthInterceptor(
             @Value("${person.service.username:default_user}") String username,
             @Value("${person.service.password:default_pass}") String password
