ff7943ba88983e8d3568341776341316773c4ea1
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 13:52:16 +0300

    add feign credentials for uth

---
+++ note-service/src/main/java/edu/joyful/noteservice/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package edu.joyful.noteservice.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    public BasicAuthRequestInterceptor feignAuthInterceptor(
+            @Value("${person.service.username:default_user}") String username,
+            @Value("${person.service.password:default_pass}") String password
+    ) {
+        return new BasicAuthRequestInterceptor(username, password);
+    }
+}
