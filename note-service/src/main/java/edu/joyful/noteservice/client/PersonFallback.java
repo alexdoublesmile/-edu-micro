c6357686ea50e8c11fdc290e1f50f675410df1f8
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 14:14:04 +0300

    add feign native fallback

---
+++ note-service/src/main/java/edu/joyful/noteservice/client/PersonFallback.java
@@ -0,0 +1,23 @@
+package edu.joyful.noteservice.client;
+
+import edu.joyful.noteservice.domain.dto.PersonDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@RefreshScope
+public class PersonFallback implements BalancedPersonClient {
+
+    @Value("${default.comment}")
+    private String DEFAULT_COMMENT;
+
+    @Override
+    public PersonDto findPersonById(Long id) {
+        return PersonDto.builder()
+                .firstName("DEFAULT_FIRST_NAME")
+                .lastName("DEFAULT_LAST_NAME")
+                .comment(DEFAULT_COMMENT)
+                .build();
+    }
+}
