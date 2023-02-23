16d6ce6ded4458ed35285eecd20a694797a4ad91
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 13:59:26 +0300

    add feign logger lvl

--- note-service/src/main/java/edu/joyful/noteservice/config/PersonFeignConfig.java
+++ note-service/src/main/java/edu/joyful/noteservice/config/PersonFeignConfig.java
@@ -1,12 +1,16 @@
 package edu.joyful.noteservice.config;
 
+import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 public class PersonFeignConfig {
 
-    
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 
     @Bean
     public BasicAuthRequestInterceptor feignAuthInterceptor(
