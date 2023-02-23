64014f5a8a2810add2e67ab872a58b4a6b3c5058
Александр Плохой <alexplohoy@mail.ru>
2023-02-23 13:57:02 +0300

    add feign explicit config for auth

--- note-service/src/main/java/edu/joyful/noteservice/client/BalancedPersonClient.java
+++ note-service/src/main/java/edu/joyful/noteservice/client/BalancedPersonClient.java
@@ -1,12 +1,13 @@
 package edu.joyful.noteservice.client;
 
 
+import edu.joyful.noteservice.config.PersonFeignConfig;
 import edu.joyful.noteservice.domain.dto.PersonDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "person-service")
+@FeignClient(name = "person-service", configuration = PersonFeignConfig.class)
 public interface BalancedPersonClient {
 
     @GetMapping("/persons/{id}")
