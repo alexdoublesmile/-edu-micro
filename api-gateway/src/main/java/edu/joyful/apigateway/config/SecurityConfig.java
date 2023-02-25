7d2043915f0cb644b8eb29589a5b29d0ee18150f
Александр Плохой <alexplohoy@mail.ru>
2023-02-25 13:23:18 +0300

    add security settings

---
+++ api-gateway/src/main/java/edu/joyful/apigateway/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package edu.joyful.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
