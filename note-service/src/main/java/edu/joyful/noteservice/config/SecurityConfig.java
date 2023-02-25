bfa5320ec74d951990962b533c7d30d55fedd666
Александр Плохой <alexplohoy@mail.ru>
2023-02-25 16:54:01 +0300

    add oauth config to note-service

---
+++ note-service/src/main/java/edu/joyful/noteservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package edu.joyful.noteservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(request -> request.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .build();
+
+    }
+}
