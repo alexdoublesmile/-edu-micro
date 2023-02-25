59bc6fd05560c64b07d60e81e60875a43f274407
Александр Плохой <alexplohoy@mail.ru>
2023-02-25 13:27:17 +0300

    fix security settings

--- api-gateway/src/main/java/edu/joyful/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/edu/joyful/apigateway/config/SecurityConfig.java
@@ -3,15 +3,17 @@ package edu.joyful.apigateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
+        return http
                 .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults())
                 .build();
