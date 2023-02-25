35d2ded6719b58f3370ab539ad4087673aa0c8b1
Александр Плохой <alexplohoy@mail.ru>
2023-02-25 17:55:05 +0300

    disable all services oauth temporally

--- person-service/src/main/java/edu/joyful/micro/config/SecurityConfig.java
+++ person-service/src/main/java/edu/joyful/micro/config/SecurityConfig.java
@@ -1,22 +1,15 @@
 package edu.joyful.micro.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests(request -> request.anyRequest().authenticated())
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                .build();
-
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeRequests(request -> request.anyRequest().authenticated())
+//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+//                .build();
+//
+//    }
 }
